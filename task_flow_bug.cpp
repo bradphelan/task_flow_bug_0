09bda088c34a03d982f3672d9200912178183e89
Brad Phelan <brad@moduleworks.com>
2023-05-03 16:17:06 +0200

    first commit

---
+++ task_flow_bug.cpp
@@ -0,0 +1,11 @@
+﻿// task_flow_bug.cpp : Defines the entry point for the application.
+//
+
+#include "task_flow_bug.h"
+#include <taskflow/algorithm/data_pipeline.hpp>
+
+
+int main()
+{
+	return 0;
+}
